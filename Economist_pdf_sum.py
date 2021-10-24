23d8b1e819c87b9448154979f8a3a0366a63cdb7
wrh8111 <48930483@qq.com>
2021-10-24 10:13:28 +0800

    20211024

---
+++ Economist_pdf_sum.py
@@ -0,0 +1,94 @@
+import camelot.io as camelot
+import pandas as pd
+import openpyxl
+import re
+import os
+
+
+class Economist_pdf_sum:
+
+    def __init__(self, pdf_file):
+        self.__temp_file = 'temp.xlsx'
+        self.pdf_file = pdf_file
+
+        if os.path.exists(self.__temp_file) == False:
+            tables = camelot.read_pdf(self.pdf_file, pages='1-end', flavor='stream')
+            res = pd.concat([table.df for table in tables], axis=0, ignore_index=True)
+            res.to_excel(self.__temp_file, encoding='utf8')
+
+    def close(self):
+        self.__clear_temp_xlsx()
+
+    #如果temp.xlsx文件存在，则删除
+    def __clear_temp_xlsx(self):
+        if os.path.exists(self.__temp_file):
+            os.remove((self.__temp_file))
+
+    #从pdf文件提取表格文本数据到temp.xlsx文件
+    # def __create_temp_xlsx(self):
+    #     if os.path.exists(self.__temp_file) == False:
+    #         tables = camelot.read_pdf(self.pdf_file, pages='1-end', flavor='stream')
+    #         res = pd.concat([table.df for table in tables], axis=0, ignore_index=True)
+    #         res.to_excel(self.__temp_file, encoding='utf8')
+
+
+    #统计PDF文件中‘姓名’字符出现的个数（通俗认为，多少个‘姓名’，就有多少人）
+    #具体实现流程：
+    #1.提取PDF文件的表格到temp.xlsx文件中
+    #2.统计temp.xlsx文件中‘姓名’字符出现的个数
+    def get_psum_from_file(self):
+        sum = 0
+        if os.path.exists(self.__temp_file):
+            wb = openpyxl.load_workbook(self.__temp_file)
+            ws = wb.active
+            for row in range(1, ws.max_row + 1):
+                for col in range(1, ws.max_column + 1):
+                    if re.match('姓名', str(ws.cell(row, col).value).strip()) != None:
+                        sum += 1
+            wb.close()
+        return sum
+
+    #统计pdf文件中每场的人数（如：第一场多少人，第二场多少人）
+    def get_psum_from_NFS(self):
+        sum = 0
+        NFC = ''
+        NFC_count = []
+        if os.path.exists(self.__temp_file):
+            wb = openpyxl.load_workbook(self.__temp_file)
+            ws = wb.active
+            for row in range(1, ws.max_row + 1):
+                for col in range(1, ws.max_column + 1):
+                    this_cell = str(ws.cell(row, col).value).strip()
+
+                    if len(re.findall(r'第\d+场', this_cell)) == 1:
+                        NFC_cell = re.findall(r'第\d+场', this_cell)[0]
+                        if NFC_cell != NFC:
+                            NFC_count.append(sum)
+                            NFC = NFC_cell
+                            sum = 0
+
+                    if re.match('姓名', this_cell) != None:
+                        sum += 1
+            wb.close()
+            NFC_count.append(sum)
+        return NFC_count[1:]
+
+
+if __name__ == '__main__':
+    # aa = Economist_pdf_sum('202110_001_1350201001.pdf')
+    # # print(aa.get_psum_from_file())
+    # print(aa.get_psum_from_NFS(),aa.get_psum_from_file())
+    # aa.close()
+    # aa = Economist_pdf_sum('202110_001_1350201008.pdf')
+    # # print(aa.get_psum_from_file())
+    # print(aa.get_psum_from_NFS(),aa.get_psum_from_file())
+    # aa.close()
+    # dir_str = 'D:\\Work\\2021年数据\\经济\\经济机考\\2021年各地市经济机考考场数据\\厦门\\[1350201]厦门技师学院_20211017124910\\全部座次表\\'
+    dir_str = os.getcwd()+'\\'
+    files = os.listdir(dir_str)
+    for file in files:
+        # print(dir_str+file)
+        if '.pdf' in file:
+            aa = Economist_pdf_sum(dir_str+file)
+            print(file[:-4],aa.get_psum_from_NFS(),aa.get_psum_from_file())
+            aa.close()
